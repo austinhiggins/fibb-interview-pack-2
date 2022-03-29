d38c9077a4f292a14879e5b23329ca6e29043424
enderdincer <ender95dincer@gmail.com>
2022-03-29 16:42:07 +0100

    Initial commit

---
+++ src/main/java/interview/technical/challenge/FibonacciCalculator.java
@@ -0,0 +1,36 @@
+package interview.technical.challenge;
+
+
+public class FibonacciCalculator {
+
+    /**
+     * Method returns the Fibonacci number at a given position.
+     *
+     * Examples:
+     *
+     * position = 0, return 0
+     * position = 1, return 1
+     * position = 2, return 1
+     * position = 3, return 2
+     * position = 4, return 3
+     * position = 5, return 5
+     * position = 6, return 8
+     * position = 7, return 13
+     * ...
+     * position = 25, return 75025
+     * ...
+     * position = 35, return 9227465
+     * ...
+     * position = 50, return 12586269025
+     * ...
+     * position = 75, return 2111485077978050
+     * ...
+     *
+     * @param position zero indexed position of a number in the Fibonacci sequence
+     * @return Fibonacci number
+     */
+    public long getFibonacciNumberAt(final int position) {
+        // TODO
+        return -1;
+    }
+}
